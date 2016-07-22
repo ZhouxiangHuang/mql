fd5a81ef72831a544789cc9b09f7688121b35f29
Zhouxiang Huang <zhouxiang.huang@gmail.com>
2016-07-22 12:46:53 +0200

    ready to development

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./client.js",
+  entry: "./src/client.js",
   output: {
     path: __dirname + "/js",
     filename: "scripts.min.js"
